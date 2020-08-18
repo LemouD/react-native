0b1391bee1873333d9b15d1ff1d05753b3e62bea
LemouD <50135939+LemouD@users.noreply.github.com>
2020-08-18 16:27:30 +0200

    Update ProductScreen.js

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -9,7 +9,7 @@ const styles = StyleSheet.create({
     title: {
         fontSize: 18,
         fontWeight: 'bold',
-        textdecoration: 'underline',
+        
     },
     stretch: {
         width: 100,
